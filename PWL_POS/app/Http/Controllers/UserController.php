8c6499de2bf62bc1e589535c18bcdfc291cee3a6
Firmank123 <firmankediri123@gmail.com>
2025-03-09 18:37:25 +0700

    week4: Not Found Exceptions
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- PWL_POS/app/Http/Controllers/UserController.php
+++ PWL_POS/app/Http/Controllers/UserController.php
@@ -20,9 +20,12 @@ class UserController extends Controller
         // UserModel::create($data);
         // $user = UserModel::all();
 
-        $user = UserModel::findor(17, ['username', 'nama'], function () {
-            abort(404);
-        });
+        // $user = UserModel::findor(17, ['username', 'nama'], function () {
+        //     abort(404);
+        // });
+        // $user = UserModel::findOrFail(1);
+
+        $user = UserModel::where('username', 'manager9')->firstOrFail();
         return view('user', ['data' => $user]);
 
         // $data = [

fbe2714d0d7d02c2e24aed0400ee2cd541175580
Firmank123 <firmankediri123@gmail.com>
2025-03-16 19:16:36 +0700

    week5: DataTables integration
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- PWL_POS/resources/views/layouts/app.blade.php
+++ PWL_POS/resources/views/layouts/app.blade.php
@@ -1,73 +1,73 @@
 @extends('adminlte::page')
 
- {{-- Extend and customize the browser title --}}
+{{-- Extend and customize the browser title --}}
 
- @section('title')
-     {{ config('adminlte.title') }}
-     @hasSection('subtitle')
-         | @yield('subtitle')
-     @endif
- @stop
+@section('title')
+    {{ config('adminlte.title') }}
+    @hasSection('subtitle')
+        | @yield('subtitle')
+    @endif
+@stop
 
- {{-- Extend and customize the page content header --}}
+@vite('resources/js/app.js')
+{{-- Extend and customize the page content header --}}
 
- @section('content_header')
-     @hasSection('content_header_title')
-         <h1 class="text-muted">
-             @yield('content_header_title')
+@section('content_header')
+    @hasSection('content_header_title')
+        <h1 class="text-muted">
+            @yield('content_header_title')
 
-             @hasSection('content_header_subtitle')
-                 <small class="text-dark">
-                     <i class="fas fa-xs fa-angle-right text-muted"></i>
-                     @yield('content_header_subtitle')
-                 </small>
-             @endif
-         </h1>
-     @endif
- @stop
+            @hasSection('content_header_subtitle')
+                <small class="text-dark">
+                    <i class="fas fa-xs fa-angle-right text-muted"></i>
+                    @yield('content_header_subtitle')
+                </small>
+            @endif
+        </h1>
+    @endif
+@stop
 
- {{-- Rename section content to content_body --}}
+{{-- Rename section content to content_body --}}
 
- @section('content')
-     @yield('content_body')
- @stop
+@section('content')
+    @yield('content_body')
+@stop
 
- {{-- Create a common footer --}}
+{{-- Create a common footer --}}
 
- @section('footer')
-     <div class="float-right">
-         Version: {{ config('app.version', '1.0.0') }}
-     </div>
+@section('footer')
+    <div class="float-right">
+        Version: {{ config('app.version', '1.0.0') }}
+    </div>
 
-     <strong>
-         <a href="{{ config('app.company_url', '#') }}">
-             {{ config('app.company_name', 'My company') }}
-         </a>
-     </strong>
- @stop
+    <strong>
+        <a href="{{ config('app.company_url', '#') }}">
+            {{ config('app.company_name', 'My company') }}
+        </a>
+    </strong>
+@stop
 
- {{-- Add common Javascript/Jquery code --}}
+{{-- Add common Javascript/Jquery code --}}
 
- @push('js')
-     <script>
-         $(document).ready(function() {
-             // Add your common script logic here...
-         });
-     </script>
- @endpush
+@push('js')
+    <script src="https://cdn.datatables.net/2.0.2/js/dataTables.js"></script>
+@endpush
 
- {{-- Add common CSS customizations --}}
+@stack('scripts')
 
- @push('css')
-     <style type="text/css">
-         {{-- You can add AdminLTE customizations here --}}
-         /*
-         .card-header {
-             border-bottom: none;
-         }
-         .card-title {
-             font-weight: 600;
-         }
-         */
-     </style>
- @endpush
+{{-- Add common CSS customizations --}}
+
+@push('css')
+    <link rel="stylesheet" href="https://cdn.datatables.net/2.0.2/css/dataTables.dataTables.css" />
+    <style type="text/css">
+        {{-- You can add AdminLTE customizations here --}}
+        /*
+             .card-header {
+                 border-bottom: none;
+             }
+             .card-title {
+                 font-weight: 600;
+             }
+             */
+    </style>
+@endpush

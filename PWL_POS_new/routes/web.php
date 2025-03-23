9ebba5d20d75ba05be8e45bb3b893683d4136335
Firmank123 <firmankediri123@gmail.com>
2025-03-24 06:25:08 +0700

    week5:PWL_POS_new: Layouting application
    Signed-off-by: Firmank123 <firmankediri123@gmail.com>

--- PWL_POS_new/routes/web.php
+++ PWL_POS_new/routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\LevelController;
 use App\Http\Controllers\KategoriController;
 use App\Http\Controllers\UserController;
+use App\Http\Controllers\WelcomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -23,9 +24,14 @@ Route::get('/', function () {
 Route::get('/level', [LevelController::class, 'index']);
 Route::get('/kategori', [KategoriController::class, 'index']);
 
-Route::get('/user', [UserController::class, 'index']);
-Route::get('/user/tambah', [UserController::class, 'tambah']);
-Route::post('/user/tambah_simpan', [UserController::class, 'tambah_simpan']);
-Route::get('/user/ubah/{id}', [UserController::class, 'edit']);
-Route::put('/user/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
-Route::get('/user/hapus/{id}', [UserController::class, 'hapus']);
+
+Route::prefix('user')->group(function () {
+    Route::get('/', [UserController::class, 'index']);
+    Route::get('/tambah', [UserController::class, 'tambah']);
+    Route::post('/tambah_simpan', [UserController::class, 'tambah_simpan']);
+    Route::get('/ubah/{id}', [UserController::class, 'edit']);
+    Route::put('/ubah_simpan/{id}', [UserController::class, 'ubah_simpan']);
+    Route::get('/hapus/{id}', [UserController::class, 'hapus']);
+});
+
+Route::get('/welcome', [WelcomeController::class, 'index']);
